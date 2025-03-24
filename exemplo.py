b283bd57284b82708e8d0eeb9161b477d257faf1
Leonardo Salin <domafras@gmail.com>
2025-03-24 19:30:32 -0300

    adiciona exemplo função

---
+++ exemplo.py
@@ -0,0 +1,9 @@
+def soma(valor1: float, valor2: float) -> float:
+    """
+    Função que soma dois valores
+    """
+    resultado = valor1 + valor2
+    return resultado
+
+resultado_soma = soma(10, 20)
+print(resultado_soma)
