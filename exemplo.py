1437c64e70ae5f588328364a86a6c5e151774475
Leonardo Salin <domafras@gmail.com>
2025-03-24 19:45:17 -0300

    removendo arquivos duplicados (nomes antigos)

--- exemplo.py
+++
@@ -1,9 +0,0 @@
-def soma(valor1: float, valor2: float) -> float:
-    """
-    Função que soma dois valores
-    """
-    resultado = valor1 + valor2
-    return resultado
-
-resultado_soma = soma(10, 20)
-print(resultado_soma)
